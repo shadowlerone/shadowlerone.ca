043825ee3532835ea6d3822e6acb1cd71f81195a
shadowlerone <andrae.lewis@gmail.com>
2020-09-07 05:28:53 -0400

    - Debug - Downloads: -- Added download endpoint

--- nodeapp/ExpressApp1/ExpressApp1/routes/downloads.js
+++ nodeapp/ExpressApp1/ExpressApp1/routes/downloads.js
@@ -1,12 +1,27 @@
 'use strict';
 
+const fs = require('fs');
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res) {
-    res.redirect('/');
-    
+	res.redirect('/');
+	
+});
+
+router.get('/album/:id/:format', function (req, res){
+	let rawdata = fs.readFileSync('routes/albums.json');
+	let albums = JSON.parse(rawdata);
+	var callback;
+	if (req.query.callback){
+		callback = req.query.callback
+	} else {
+		callback = `/discography/${req.params.id}`
+	}
+	var album = albums['albums'][req.params.id]['title'];
+	res.download(`${__dirname}/../downloads/albums/${req.params.format}/${album}.zip`);
+	res.redirect(`/${callback}`);
 });
 
 module.exports = router;

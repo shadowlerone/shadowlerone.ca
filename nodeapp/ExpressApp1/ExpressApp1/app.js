882133d4affc39f55115b0256e9b86a9d7d1910d
shadowlerone <andrae.lewis@gmail.com>
2020-09-07 03:25:09 -0400

    added downloads to the routes

--- nodeapp/ExpressApp1/ExpressApp1/app.js
+++ nodeapp/ExpressApp1/ExpressApp1/app.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var downloads = require('./routes/downloads');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/downloads', downloads);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ nodeapp/ExpressApp1/ExpressApp1/routes/downloads.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res) {
+    
+    
+});
+
+module.exports = router;
